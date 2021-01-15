5c83f1bfc9775f4930bafdaa4d125c3290f4e454
PeterDing <dfhayst@gmail.com>
2021-01-15 12:34:52 +0800

    Add common/progress_bar.py

---
+++ baidupcs_py/common/progress_bar.py
@@ -0,0 +1,22 @@
+from rich.progress import (
+    Progress,
+    SpinnerColumn,
+    TextColumn,
+    BarColumn,
+    DownloadColumn,
+    TransferSpeedColumn,
+    TimeRemainingColumn,
+)
+
+_progress = Progress(
+    SpinnerColumn(),
+    TextColumn("[bold blue]{task.fields[title]}", justify="right"),
+    BarColumn(bar_width=40),
+    "[progress.percentage]{task.percentage:>3.1f}%",
+    "•",
+    DownloadColumn(),
+    "•",
+    TransferSpeedColumn(),
+    "•",
+    TimeRemainingColumn(),
+)
