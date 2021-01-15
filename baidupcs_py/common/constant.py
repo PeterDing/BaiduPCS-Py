26095ad7c40c001324efd8ed2d162768065141b1
PeterDing <dfhayst@gmail.com>
2021-01-15 12:27:06 +0800

    Add common/constant.py

---
+++ baidupcs_py/common/constant.py
@@ -0,0 +1,11 @@
+import os
+
+CPU_NUM = os.cpu_count() or 1
+
+# Defines that should never be changed
+OneK = 1024
+OneM = OneK * OneK
+OneG = OneM * OneK
+OneT = OneG * OneK
+OneP = OneT * OneK
+OneE = OneP * OneK
