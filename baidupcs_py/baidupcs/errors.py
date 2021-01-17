d92fb5873f370834ce2a9d03bf90a63ca7eccc81
PeterDing <dfhayst@gmail.com>
2021-01-17 15:53:32 +0800

    Add error

--- baidupcs_py/baidupcs/errors.py
+++ baidupcs_py/baidupcs/errors.py
@@ -102,6 +102,7 @@ ERRORS = {
     31299: "服务器返回值KEY不存在",
     31304: "file type is not supported",
     31341: "be transcoding, please wait and retry",
+    36001: "离线下载错误",
     36032: "hit sexy spam",
     36037: "source url not support",
 }
