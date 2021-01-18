83e0d4fbf939f79a506045cac76dbc6dc3eb93c7
PeterDing <dfhayst@gmail.com>
2021-01-18 21:55:23 +0800

    Add calu_file_md5 which uses system md5 bin

--- baidupcs_py/common/crypto.py
+++ baidupcs_py/common/crypto.py
@@ -1,8 +1,34 @@
-from typing import Union, Tuple
+from typing import Union, List, Tuple
+import re
 import io
+import sys
 from zlib import crc32
-
 from hashlib import md5, sha1
+import subprocess
+
+
+def _md5_cmd(localpath: str) -> List[str]:
+    if sys.platform == "darwin":
+        cmd = ["md5", localpath]
+    elif sys.platform == "linux":
+        cmd = ["md5sum", localpath]
+    else:  # windows
+        cmd = ["CertUtil", "-hashfile", localpath, "MD5"]
+    return cmd
+
+
+def calu_file_md5(localpath: str) -> str:
+    cp = subprocess.run(
+        _md5_cmd(localpath), universal_newlines=True, stdout=subprocess.PIPE
+    )
+
+    output = cp.stdout.strip()
+    if sys.platform == "darwin":
+        return re.split(r"\s+", output)[-1]
+    elif sys.platform == "linux":
+        return re.split(r"\s+", output)[0]
+    else:  # windows
+        return re.split(r"\s+", output)[-6]
 
 
 def calu_md5(buf: Union[str, bytes], encoding="utf-8") -> str:
