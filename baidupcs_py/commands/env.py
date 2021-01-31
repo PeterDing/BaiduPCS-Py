5f5d5b12a692b63bd59f686f576a1c1a0cb5d85d
PeterDing <dfhayst@gmail.com>
2021-01-31 22:28:40 +0800

    Add commands/env.py

---
+++ baidupcs_py/commands/env.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+# Local data directory
+DATA_DIR = Path("~").expanduser() / ".baidupcs-py"
+
+# Account data path
+ACCOUNT_DATA_PATH = DATA_DIR / "accounts.pk"
+
+# Logging path
+LOG_PATH = DATA_DIR / "running.log"
+
+# Logging level
+LOG_LEVEL = "CRITICAL"
