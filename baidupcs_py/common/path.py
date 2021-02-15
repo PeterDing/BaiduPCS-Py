c298e1278d0992432ea9d7a746d74bd6bf61469b
PeterDing <dfhayst@gmail.com>
2021-02-15 12:18:40 +0800

    Compatible for Windows

--- baidupcs_py/common/path.py
+++ baidupcs_py/common/path.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import os
 from os import PathLike
 
+from baidupcs_py.common.platform import IS_WIN
+
 
 def exists(localpath: PathLike) -> bool:
     localpath = Path(localpath)
@@ -27,8 +29,25 @@ def walk(localpath: PathLike) -> Iterator[str]:
 
 
 def join_path(source: PathLike, dest: PathLike) -> str:
+    """Join posix paths
+
+    Only resolve relative path.
+    """
+
+    has_root = Path(source).as_posix().startswith("/")
+
     if not isinstance(source, Path):
         source = Path(source)
     if not isinstance(dest, Path):
         dest = Path(dest)
-    return (source / dest).resolve().as_posix()
+
+    path = (source / dest).resolve().as_posix()
+
+    if not has_root:
+        parents = Path("").resolve().as_posix()
+        path = path[len(parents) + 1 :]
+
+    if IS_WIN and has_root:
+        return path.split(":", 1)[-1]
+    else:
+        return path
