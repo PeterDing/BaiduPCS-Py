146a872e9e2d42ca6d6dd2d86ffa6f224ab86e4f
PeterDing <dfhayst@gmail.com>
2021-04-16 11:45:49 +0800

    Add func `human_size_to_int`

--- baidupcs_py/utils.py
+++ baidupcs_py/utils.py
@@ -1,5 +1,7 @@
 import json
 import time
+import string
+import math
 
 
 def dump_json(obj) -> str:
@@ -22,3 +24,37 @@ def human_size(size: int) -> str:
     if v.endswith(".0"):
         v = v[:-2]
     return f"{v} {t}"
+
+
+_nums_set = set(string.digits + ".")
+
+
+def human_size_to_int(size_str: str) -> int:
+    size_str = size_str.strip()
+    if not size_str:
+        return 0
+
+    i = 0
+    while i < len(size_str):
+        if size_str[i] in _nums_set:
+            i += 1
+            continue
+        else:
+            break
+
+    if i == 0:
+        return 0
+
+    s = float(size_str[:i])
+    _s = s
+
+    unit = size_str[i:].upper().replace(" ", "")
+    if not unit:
+        return math.floor(_s)
+
+    for t in ["KB", "MB", "GB", "TB"]:
+        _s *= 1024
+        if unit == t or unit[0] == t[0]:
+            return math.floor(_s)
+
+    return math.floor(s)
